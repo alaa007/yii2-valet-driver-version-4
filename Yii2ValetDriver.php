ddccd294d2ee37fab4ea9f338770e05bf9844a13
Alaa AH <alaa007@gmail.com>
2023-03-16 10:55:00 +0300

    Update Yii2ValetDriver.php

--- Yii2ValetDriver.php
+++ Yii2ValetDriver.php
@@ -1,5 +1,9 @@
 <?php
 
+namespace Valet\Drivers\Custom;
+
+use Valet\Drivers\ValetDriver;
+
 class Yii2ValetDriver extends ValetDriver
 {
     /**
@@ -10,7 +14,7 @@ class Yii2ValetDriver extends ValetDriver
      * @param  string  $uri
      * @return bool
      */
-    public function serves($sitePath, $siteName, $uri)
+    public function serves(string $sitePath, string $siteName, string $uri): bool
     {
         if (file_exists($sitePath.'/../vendor/yiisoft/yii2/Yii.php') || file_exists($sitePath.'/vendor/yiisoft/yii2/Yii.php')) {
             return true;
@@ -27,15 +31,10 @@ class Yii2ValetDriver extends ValetDriver
      * @param  string  $uri
      * @return string|false
      */
-    public function isStaticFile($sitePath, $siteName, $uri)
+    public function isStaticFile(string $sitePath, string $siteName, string $uri)/*: string|false */
     {
-        // this works for domains called code assets
-        // for example your site name product.{valet-domen} assets domen is assets.product.{valet-domen}
-        if(preg_match("#^assets#", $siteName) ) {
-            return $sitePath.$uri;
-        }
-        
-        if (file_exists($staticFilePath = $sitePath.'/web/'.$uri) && ! is_dir ( $staticFilePath ) && pathinfo ( $staticFilePath )['extension'] != '.php') {
+    
+        if (file_exists($staticFilePath = $sitePath.'/frontend/web/'.$uri) && !is_dir($sitePath.'/frontend/web/'.$uri)) {
             return $staticFilePath;
         }
 
@@ -50,28 +49,49 @@ class Yii2ValetDriver extends ValetDriver
      * @param  string  $uri
      * @return string
      */
-    public function frontControllerPath($sitePath, $siteName, $uri)
+    public function frontControllerPath(string $sitePath, string $siteName, string $uri): string
     {
+         if(strpos($uri, '/backend/filemanager')!==false){
+            $_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'];
+            $_SERVER['SCRIPT_FILENAME'] = $sitePath.'/frontend/web/backend/filemanager/dialog.php';
+            $_SERVER['SCRIPT_NAME'] = '/backend/filemanager/dialog.php';
+            $_SERVER['PHP_SELF'] = '/backend/filemanager/dialog.php';
+            $_SERVER['DOCUMENT_ROOT'] = $sitePath;
+             return $sitePath.'/frontend/web/backend/filemanager/dialog.php';
+        }
 
-        $uri_path = explode('/',$uri)[1];
+        if(strpos($uri, '/backend')!==false){
+            $_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'];
+            $_SERVER['SCRIPT_FILENAME'] = $sitePath.'/frontend/web/backend/index.php';
+            $_SERVER['SCRIPT_NAME'] = '/backend/index.php';
+            $_SERVER['PHP_SELF'] = '/backend/index.php';
+            $_SERVER['DOCUMENT_ROOT'] = $sitePath;
+            //if (strpos($_SERVER['REQUEST_URI'], '/backend') === 0) {
+              //  $_SERVER['REQUEST_URI'] = substr($_SERVER['REQUEST_URI'], 8);
+                //var_dump($_SERVER['REQUEST_URI']);
+                // /die;
+            //}
+            return $sitePath.'/frontend/web/backend/index.php';
+        }elseif(strpos($uri, '/employer')!==false){
+            $_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'];
+            $_SERVER['SCRIPT_FILENAME'] = $sitePath.'/frontend/web/employer/index.php';
+            $_SERVER['SCRIPT_NAME'] = '/employer/index.php';
+            $_SERVER['PHP_SELF'] = '/employer/index.php';
+            $_SERVER['DOCUMENT_ROOT'] = $sitePath;
+            //if (strpos($_SERVER['REQUEST_URI'], '/backend') === 0) {
+              //  $_SERVER['REQUEST_URI'] = substr($_SERVER['REQUEST_URI'], 8);
+                //var_dump($_SERVER['REQUEST_URI']);
+                // /die;
+            //}
+            return $sitePath.'/frontend/web/employer/index.php';
+        }else{
+            $_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'];
+            $_SERVER['SCRIPT_FILENAME'] = $sitePath.'/frontend/web/index.php';
+            $_SERVER['SCRIPT_NAME'] = '/index.php';
+            $_SERVER['PHP_SELF'] = '/index.php';
+            $_SERVER['DOCUMENT_ROOT'] = $sitePath;
 
-         
-        if (file_exists($sitePath.'/web/'. $uri_path . '/index.php') && !empty($uri_path)) {
-
-           $_SERVER['SCRIPT_FILENAME'] = $sitePath.'/web/' . $uri_path . '/index.php';
-           $_SERVER['SCRIPT_NAME'] = '/' . $uri_path . '/index.php';
-           $_SERVER['PHP_SELF'] = '/' . $uri_path . '/index.php';
-           $_SERVER['DOCUMENT_ROOT'] = $sitePath;
-
-           return $sitePath.'/web/' . $uri_path . '/index.php';
-       }
-
-        $_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'];
-        $_SERVER['SCRIPT_FILENAME'] = $sitePath.'/web/index.php';
-        $_SERVER['SCRIPT_NAME'] = '/index.php';
-        $_SERVER['PHP_SELF'] = '/index.php';
-        $_SERVER['DOCUMENT_ROOT'] = $sitePath;
-
-        return $sitePath.'/web/index.php';
+            return $sitePath.'/frontend/web/index.php';
+        }
     }
 }
